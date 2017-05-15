59044d79edd9a6bfd969b6a9bb5e19ea994c6698
alberto <apascualco@gmail.com>
2017-05-15 06:54:55 +0200

    Initial commit

---
+++ src/main/java/apascualco/blog/basics/Comentarios.java
@@ -0,0 +1,21 @@
+package apascualco.blog.basics;
+
+/**
+ * Javadoc
+ * @author Alberto Pascual
+ */
+public class Comentarios {
+
+    public static void main(String[] args) {
+        // Comentario en una sola línea -> Imprime hola
+        System.out.println("Hello world");
+    }
+
+    /*
+    * Comantarios de múltiples líneas
+    * Método que devuelve hola
+    */
+    private static String diHola() {
+        return "hola";
+    }
+}
